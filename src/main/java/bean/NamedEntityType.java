c95ed7a268ba5116c7f68e67d9d5b01e46dcb0d7
黄伟成 <5145813@qq.com>
2026-05-10 19:51:19 +0800

    Refactor code structure for improved readability and maintainability

--- src/main/java/bean/NamedEntityType.java
+++
@@ -1,9 +0,0 @@
-package bean;
-
-public interface NamedEntityType {
-	public String PERSON = "PERSON";
-	public String ORGANIZATION = "ORGANIZATION";
-	public String LOCATION = "LOCATION";
-	public String NUMBER = "NUMBER";
-	public String O = "O";
-}
--- src/main/java/exporter/Exporter.java
+++
@@ -1,7 +0,0 @@
-package exporter;
-
-import table.Table;
-
-public interface Exporter {
-	public void export(Table[] table, String filePath);
-}
