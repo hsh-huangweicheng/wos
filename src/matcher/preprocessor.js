2b9f9e974dff409b7a5b353576ed7c382f4cf328
黄伟成 <5145813@qq.com>
2026-05-27 21:47:42 +0800

    update

--- src/matcher/preprocessor.js
+++
@@ -1,6 +0,0 @@
-function preprocessForMatch(str) {
-  if (!str) return "";
-  return str.toLowerCase().replace(/[^a-z0-9 ]/g, " ").replace(/\s+/g, " ").trim();
-}
-
-module.exports = { preprocessForMatch };
